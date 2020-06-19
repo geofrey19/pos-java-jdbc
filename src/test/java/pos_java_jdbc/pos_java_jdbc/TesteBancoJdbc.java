013a135d1a52e5e32c6272b81851f0e12000b914
George Frey <geofrey19@gmail.com>
2020-06-19 09:17:03 -0300

    Criando INSERT no banco de dados

--- src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
+++ src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -2,13 +2,22 @@ package pos_java_jdbc.pos_java_jdbc;
 
 import org.junit.Test;
 
-import conexaojdbc.SingleConnection;
+import dao.UserPosDAO;
 import junit.framework.TestCase;
+import model.Userposjava;
 
 public class TesteBancoJdbc {
 
 	@Test
 	public void initBanco() {
-		SingleConnection.getConnection();
+		UserPosDAO userPosDAO = new UserPosDAO();
+		Userposjava userposjava = new Userposjava();
+		
+		userposjava.setId(4L);
+		userposjava.setNome("Suzana Frey");
+		userposjava.setEmail("sufrey@yaho.com.br");
+		
+		userPosDAO.Salvar(userposjava);
+		
 	}
 }
